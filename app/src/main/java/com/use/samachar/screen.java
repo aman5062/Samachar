6687d786c8231098c8748e99b31de8c92211d963
aman5062 <aman9122701848@gmail.com>
2024-09-19 11:13:19 +0530

    s

--- app/src/main/java/com/use/samachar/screen.java
+++ app/src/main/java/com/use/samachar/screen.java
@@ -2,13 +2,39 @@ package com.use.samachar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class screen extends AppCompatActivity {
+    ImageView menu_icon;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+        getSupportActionBar().hide();
+        menu_icon = findViewById(R.id.menu_icon);
+
+        menu_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = new Dialog(screen.this);
+                dialog.setContentView(R.layout.menu);
+                dialog.setCancelable(true);
+                dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+                dialog.getWindow().getAttributes().windowAnimations = R.style.Theme_Samachar;
+                dialog.getWindow().setBackgroundDrawableResource(android.R.drawable.screen_background_light_transparent);
+
+                dialog.show();
+            }
+        });
+
+
     }
 }
